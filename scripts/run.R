b24b2dcf7855a243a4514b518dbe538086d54e40
almac2022 <al@newgraphenvironment.com>
2024-05-13 16:12:48 -0700

    add news to appendix to build.  move to new pipe

--- scripts/run.R
+++ scripts/run.R
@@ -11,10 +11,14 @@ preview_chapter('0100-intro.Rmd')
 # if you need to update after renaming photos to keep and add to map, or if you added new photos....
 source('scripts/02_reporting/0180-photos-extract-metadata.R')
 
+
+
 {
+  source('scripts/functions.R')
+  news_to_appendix()
   # These files are included in the gitbook version already so we move them out of the build
-  files_to_move <- list.files(pattern = ".Rmd$") %>%
-    stringr::str_subset(., '2300', negate = F) #move the attachments out
+  files_to_move <- list.files(pattern = ".Rmd$") |>
+    stringr::str_subset('2300', negate = F) #move the attachments out
   files_destination <- paste0('hold/', files_to_move)
 
   ##move the files
